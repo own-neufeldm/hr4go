71571b62e343017f6820da222f34ba3110161d7c
Martin Neufeld <m.neufeld946@gmail.com>
2024-08-24 01:28:09 +0200

    ci: use github module ref

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module hr4go
+module github.com/own-neufeldm/hr4go
 
 go 1.23.0
--- main.go
+++ main.go
@@ -3,8 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
-	"hr4go/lib"
 	"strings"
+
+	"github.com/own-neufeldm/hr4go/lib"
 )
 
 func usage() {
